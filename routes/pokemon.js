21319a8855ee9d0d7c9f9d7456d4e6e8f154ed7b
unknown <jt.mccutcheon@live.com>
2020-05-01 01:50:19 -0400

    add endpoints for cards by type

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -2,6 +2,10 @@ const pokemon = require('pokemontcgsdk'); // <--------https://github.com/Pokemon
 const router = require('express').Router();
 const cardData = require('../data');
 // console.log(cardData);
+
+// @route     GET api/v1/pokemon
+// @desc      Get all cards
+// @access    public
 router.get('/', async (req, res) => {
 	try {
 		// let base1Pokemon = await pokemon.card.where({
@@ -23,6 +27,128 @@ router.get('/', async (req, res) => {
 		console.log(error.message);
 		res.status(500).send('server error');
 	}
+});
+
+// @route     GET api/v1/pokemon/grass
+// @desc      Get all grass pokemon cards
+// @access    public
+router.get('/grass', (req, res) => {
+	let pokemon = [];
+	for (let i = 0; i < cardData.length; i++) {
+		if (cardData[i].types) pokemon.push(cardData[i]);
+	}
+
+	let grass = pokemon.filter((grass) => grass.types[0] === 'Grass');
+
+	res.status(200).json(grass);
+});
+
+// @route     GET api/v1/pokemon/fire
+// @desc      Get all fire pokemon cards
+// @access    public
+router.get('/fire', (req, res) => {
+	let pokemon = [];
+	for (let i = 0; i < cardData.length; i++) {
+		if (cardData[i].types) pokemon.push(cardData[i]);
+	}
+
+	let fire = pokemon.filter((fire) => fire.types[0] === 'Fire');
+
+	res.status(200).json(fire);
+});
+
+// @route     GET api/v1/pokemon/psychic
+// @desc      Get all psychic pokemon cards
+// @access    public
+router.get('/psychic', (req, res) => {
+	let pokemon = [];
+	for (let i = 0; i < cardData.length; i++) {
+		if (cardData[i].types) pokemon.push(cardData[i]);
+	}
+
+	let psychic = pokemon.filter((psychic) => psychic.types[0] === 'Psychic');
+
+	res.status(200).json(psychic);
+});
+
+// @route     GET api/v1/pokemon/water
+// @desc      Get all water pokemon cards
+// @access    public
+router.get('/water', (req, res) => {
+	let pokemon = [];
+	for (let i = 0; i < cardData.length; i++) {
+		if (cardData[i].types) pokemon.push(cardData[i]);
+	}
+	let water = pokemon.filter((water) => water.types[0] === 'Water');
+
+	res.status(200).json(water);
+});
+
+// @route     GET api/v1/pokemon/colorless
+// @desc      Get all colorless pokemon cards
+// @access    public
+router.get('/colorless', (req, res) => {
+	let pokemon = [];
+	for (let i = 0; i < cardData.length; i++) {
+		if (cardData[i].types) pokemon.push(cardData[i]);
+	}
+	let colorless = pokemon.filter(
+		(colorless) => colorless.types[0] === 'Colorless',
+	);
+
+	res.status(200).json(colorless);
+});
+
+// @route     GET api/v1/pokemon/fighting
+// @desc      Get all fighting pokemon cards
+// @access    public
+router.get('/fighting', (req, res) => {
+	let pokemon = [];
+	for (let i = 0; i < cardData.length; i++) {
+		if (cardData[i].types) pokemon.push(cardData[i]);
+	}
+	let fighting = pokemon.filter(
+		(fighting) => fighting.types[0] === 'Fighting',
+	);
+
+	res.status(200).json(fighting);
+});
+
+// @route     GET api/v1/pokemon/lightning
+// @desc      Get all fighting pokemon cards
+// @access    public
+router.get('/lightning', (req, res) => {
+	let pokemon = [];
+	for (let i = 0; i < cardData.length; i++) {
+		if (cardData[i].types) pokemon.push(cardData[i]);
+	}
+	let lightning = pokemon.filter(
+		(lightning) => lightning.types[0] === 'Lightning',
+	);
+
+	res.status(200).json(lightning);
+});
+
+// @route     GET api/v1/pokemon/trainer
+// @desc      Get all trainer cards
+// @access    public
+router.get('/trainer', (req, res) => {
+	let trainerCards = [];
+	for (let i = 0; i < cardData.length; i++) {
+		if (cardData[i].supertype === 'Trainer') trainerCards.push(cardData[i]);
+	}
+	res.status(200).json(trainerCards);
+});
+
+// @route     GET api/v1/pokemon/energy
+// @desc      Get all energy cards
+// @access    public
+router.get('/energy', (req, res) => {
+	let energyCards = [];
+	for (let i = 0; i < cardData.length; i++) {
+		if (cardData[i].supertype === 'Energy') energyCards.push(cardData[i]);
+	}
+	res.status(200).json(energyCards);
 });
 
 module.exports = router;
