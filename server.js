d7c07aef2e45820cf1571c2de97fc82c7846f192
unknown <jt.mccutcheon@live.com>
2020-09-12 13:42:56 -0400

    add me to env

--- server.js
+++ server.js
@@ -1,37 +1,36 @@
-const express = require("express");
-require("dotenv").config();
+const express = require('express');
+require('dotenv').config();
 // const cors = require("cors");
 // database connection file
-const dbConnect = require("./dbConnect");
+const dbConnect = require('./dbConnect');
 // route files
 const userRoutes = require('./routes/user');
 const deckRoutes = require('./routes/deck');
 const PokemonRoutes = require('./routes/pokemon');
 const RoomChat = require('./routes/RoomChat');
-const chatRoutes = require("./routes/chat");
+const chatRoutes = require('./routes/chat');
 // initalize express
 const app = express();
 const socketTest = require('./routes/socketTest');
 
 // console.log(cors);
 // init middleware
-let cors = function(req, res, next) {
-  var whitelist = [
-    'http://localhost:4200',
-    'https://allegedlytcg.com',
-  ];
-  let origin = req.headers.origin;
-  if (whitelist.indexOf(origin) > -1) {
-    res.setHeader('Access-Control-Allow-Origin', origin);
-  }
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Authorization');
-  next();
-}
-app.use(cors);
-
-
+let cors = function (req, res, next) {
+	var whitelist = [
+		'http://localhost:4200',
+		'https://allegedlytcg.com',
+		process.env.KEY_IP,
+	];
+	let origin = req.headers.origin;
+	if (whitelist.indexOf(origin) > -1) {
+		res.setHeader('Access-Control-Allow-Origin', origin);
+	}
+	res.setHeader('Access-Control-Allow-Credentials', true);
+	res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+	res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+	next();
+};
+app.use(cors());
 
 // const corsOptions = {
 //   origin: "http://localhost:4200", // TODO for sure change to deployed frontend link later
@@ -50,19 +49,19 @@ app.use('/api/v1/deck', deckRoutes);
 app.use('/api/v1/pokemon', PokemonRoutes);
 app.use('/api/v1/socketTest', socketTest);
 app.use('/api/v1/RoomChat', RoomChat);
-app.use("/api/v1/chat", chatRoutes);
+app.use('/api/v1/chat', chatRoutes);
 
 const PORT = process.env.PORT || 6000;
 
 server = app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+	console.log(`App listening on port ${PORT}!`);
 });
 
-const io = require("socket.io")(
-  server,
+const io = require('socket.io')(
+	server,
 
-  // 	{
-  //   transports: ["websocket", "polling"],
-  // }
+	// 	{
+	//   transports: ["websocket", "polling"],
+	// }
 );
-app.set("io", io);
+app.set('io', io);
